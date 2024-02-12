6848af53262e680a24f54a14558c0819c33cd3a2
pantcr7 <prasantpant141@gmail.com>
2024-02-13 00:01:05 +0545

    scroll and list tested

--- App.tsx
+++ App.tsx
@@ -1,77 +1,57 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button,TextInput} from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
 
-const HelloWorldApp = () => {
-  const [name, setName] = useState("Prasant");
-  const [person,setPerson] = useState({name:'Nisant',age:27});
+const app = () =>{
+const [people, setPeople] = useState([
+  {name: 'Shyam', key: '1'},
+  {name: 'Radhe', key: '2'},
+  {name: 'Krishna', key: '3'},
+  {name: 'Hare', key: '4'},
+  {name: 'Ram', key: '5'},
+  {name: 'Shyam', key: '6'},
+  {name: 'Radhe', key: '7'},
+  {name: 'Krishna', key: '8'},
+  {name: 'Hare', key: '9'},
+  {name: 'Ram', key: '10'},
+  {name: 'Shyam', key: '11'},
+  {name: 'Radhe', key: '12'},
+  {name: 'Krishna', key: '13'},
+  {name: 'Hare', key: '14'},
+  {name: 'Ram', key: '15'},
+])
+return(
+  <View style = {styles.container}>
+    <ScrollView showsVerticalScrollIndicator={false}>
+    {/* {people.map((item) =>{
+      return(
+        <View key={item.key}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>
+      )
+    })} */}
 
-  const clickHandler = () =>{
-    setName("Memika")
-    setPerson({name:'Prasant',age:33})
-  }
-  return (
-    <View style = {styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>{name} Pant</Text>
-        <Text style={styles.boldText}>{person.name} is of {person.age}</Text>
-      </View>
-      <View style={styles.subContainer}>
-        <Text >Enter Name:</Text>
-        <TextInput 
-        multiline
-        style = {styles.input}
-        placeholder='e.g John Wick'
-        textAlign='center'
-        onChangeText={(value) => setPerson({...person, name: value})} />
-
-      <Text >Enter Age:</Text>
-        <TextInput 
-        keyboardType='number-pad'
-        style = {styles.input}
-        placeholder='e.g 24'
-        textAlign='center'
-        onChangeText={(val) => setPerson({...person, age: parseInt(val)})} />
-      </View>
-      {/* <View style={[styles.header, styles.gap]}>
-        <Text style={styles.boldText}>Is Going To Hell</Text>
-      </View> */}
-      <View style={styles.buttonContainer}>
-        <Button title='Update Name' onPress={clickHandler} />
-      </View>
-    </View>
-  );
-};
-export default HelloWorldApp;
+      {people.map(item => (
+        <View key={item.key}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>
+      
+    ))}
+    </ScrollView>
+  </View>
+)
+}
+export default app;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex:1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor:'#fff',
+    paddingHorizontal:20
   },
-  subContainer:{
-    alignItems:'center'
-  },
-  header: {
+  item:{
+    marginTop:20,
     backgroundColor:'pink',
-    padding:20,
-    borderRadius: 10
-  },
-  boldText:{
-    fontWeight: 'bold'
-  },
-  gap:{
-    marginTop:10
-  },
-  buttonContainer:{
-    marginTop:10
-  },
-  input:{
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    borderWidth:1,
-    width: 200
+    fontSize:20,
+    padding:20
   }
 })
