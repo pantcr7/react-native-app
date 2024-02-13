4bace82843763635166f68e1a408afc32c8a16a8
pantcr7 <prasantpant141@gmail.com>
2024-02-13 22:42:17 +0545

    todo app simplified

--- App.tsx
+++ App.tsx
@@ -1,73 +1,53 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, FlatList, Dimensions,TouchableOpacity } from "react-native";
+import React,{ useState} from "react";
+import { StyleSheet,Text, View,FlatList } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoiitem";
+const app = () => {
+  const [todo, setTodo] = useState([
+    {text:"buy coffee", key:'1'},
+    {text:"buy pen", key:'2'},
+    {text:"buy mobile", key:'3'},
+    {text:"buy book", key:'4'},
+    {text:"buy tea", key:'5'},
+  ])
+  const pressHandler = (key: string) => {
+    setTodo((prev) => {
+      return prev.filter(todo => todo.key != key)
+    })
+  }
+  return (
+    <View style={styles.container}>
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todo}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler = {pressHandler} />
+            )}
 
-const app = () =>{
-const [people, setPeople] = useState([
-  //works fine for key but for id, it works differently using
-  {name: 'Shyam', id: '1'},
-  {name: 'Radhe', id: '2'},
-  {name: 'Krishna', id: '3'},
-  {name: 'Hare', id: '4'},
-  {name: 'Ram', id: '5'},
-  {name: 'Shyam', id: '6'},
-  {name: 'Radhe', id: '7'},
-  {name: 'Krishna', id: '8'},
-  {name: 'Hare', id: '9'},
-  {name: 'Ram', id: '10'},
-  {name: 'Shyam', id: '11'},
-  {name: 'Radhe', id: '12'},
-  {name: 'Krishna', id: '13'},
-  {name: 'Hare', id: '14'},
-  {name: 'Ram', id: '15'},
-])
+          />
+        </View>
 
-const pressHanddler = (id: string) =>{
-  console.log(id)
-  setPeople((prev) => {
-    return prev.filter(person => person.id != id)
-  })
-}
-return(
-  <View style = {styles.container}>
-    
-   <FlatList
-   numColumns={3}
-   keyExtractor={(item) => item.id}
-   data={people}
-   renderItem={({ item }) => (
-    <TouchableOpacity onPress={() => pressHanddler(item.id)}>
-    <View style={styles.itemContainer}>
-    <Text style={styles.item}>{item.name}</Text>
-    
-  </View>
-  </TouchableOpacity>
-   )}
-   />
-    
-  </View>
-)
+      </View>
+    </View>
+  )
+
 }
 export default app;
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex:1,
     backgroundColor:'#fff',
-    paddingHorizontal:20
   },
-  itemContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 5,
-    width: (Dimensions.get('window').width - 60) / 3, // Adjust width for 3 columns
-    backgroundColor: 'pink',
+  content:{
+    marginTop:20,
+    padding:20
   },
-  item:{
-    
-    fontSize:15,
-    padding:10,
- 
-    
-  }
+  list:{
+
+  },
 })
